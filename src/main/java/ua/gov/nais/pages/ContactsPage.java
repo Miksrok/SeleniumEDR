cfbc3f23ff6425fe22aa6e499db45669599b350a
Miksrok <holderok@gmail.com>
2018-10-24 14:29:16 +0300

    add contacts page

---
+++ src/main/java/ua/gov/nais/pages/ContactsPage.java
@@ -0,0 +1,18 @@
+package ua.gov.nais.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class ContactsPage {
+
+    private WebDriver driver;
+
+    private WebElement phoneNumber;
+
+    public ContactsPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+}
